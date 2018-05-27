52052b81eea76192914d81ee1be59b3ed5f6e2d4
Luis Fernando Contreras Guerra <luisguerra495@gmail.com>
2018-05-26 23:57:09 -0500

    Add firebase

--- app/src/main/java/me/luisguerra495/noteapp/MainActivity.kt
+++ app/src/main/java/me/luisguerra495/noteapp/MainActivity.kt
@@ -2,11 +2,33 @@ package me.luisguerra495.noteapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings.Global.getString
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), GoogleApiClient.OnConnectionFailedListener {
+
+    var googleApiClient: GoogleApiClient? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id)).
+                        requestEmail()
+                        .build()
+
+        googleApiClient = GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build()
+    }
+
+    override fun onConnectionFailed(p0: ConnectionResult) {
+        
     }
 }
